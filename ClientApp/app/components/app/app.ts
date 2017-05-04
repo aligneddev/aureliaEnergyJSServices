315fb5d70b3b9576c0a5f8fa9348eb3eec949083
Kevin Logan <logk34@hotmail.com>
2017-05-04 13:40:50 -0500

    master details is working

--- ClientApp/app/components/app/app.ts
+++ ClientApp/app/components/app/app.ts
@@ -8,8 +8,7 @@ export class Home {
     config.map([
       // http://stackoverflow.com/questions/40348535/organize-by-folder-in-aurelia
        { route: ['', 'energy'], name: 'energy', moduleId: '../energy/energy', nav: true, title: 'Energy' },
-    //   { route: 'energy-details', name: 'energy-details',
-    //     moduleId: 'energy/energyDetailsView', nav: false, title: 'Energy Details'}
+       { route: 'energy-details', name: 'energy-details', moduleId: '../energy/energyDetailsView', nav: false, title: 'Energy Details'}
     ]);
 
     this.router = router;
